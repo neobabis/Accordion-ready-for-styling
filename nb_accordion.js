8df01aa5a6b3dd9b2f7d7baa3637c04048c1f961
neobabis <48261152+neobabis@users.noreply.github.com>
2021-06-12 17:11:46 +0300

    Accordion v1.0

---
+++ nb_accordion.js
@@ -0,0 +1,36 @@
+// ------------------------------
+// Author: Neokazis Charalampos
+// Author URI: NeoBabis.gr
+// Version: 1.0
+// ------------------------------
+
+const nb_accordions = document.querySelectorAll("[data-nb_accordion]");
+
+if (nb_accordions) {
+    nb_accordions.forEach((nb_accordion) => {
+        let labels = nb_accordion.querySelectorAll("[data-nb_accordion_label]");
+
+        labels.forEach((label) => {
+            label.addEventListener("click", () => {
+                if (nb_accordion.hasAttribute("data-nb_accordion_open_only_one")) {
+                    let all_active = nb_accordion.querySelectorAll("[data-nb_accordion_item].active");
+                    all_active.forEach((e) => {
+                        if (e != label.closest("[data-nb_accordion_item]")) {
+                            e.classList.remove("active");
+                            e.querySelector("[data-nb_accordion_label]").nextElementSibling.style.maxHeight = "0px";
+                        }
+                    });
+                }
+
+                let closest_accordion_item = label.closest("[data-nb_accordion_item]");
+                if (closest_accordion_item.classList.contains("active")) {
+                    closest_accordion_item.classList.remove("active");
+                    label.nextElementSibling.style.maxHeight = "0px";
+                } else {
+                    closest_accordion_item.classList.add("active");
+                    label.nextElementSibling.style.maxHeight = `${label.nextElementSibling.scrollHeight}px`;
+                }
+            });
+        });
+    });
+}
